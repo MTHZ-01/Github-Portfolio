f2ddc9d553c60b6a2f5a21a6a25748d41f326b96
unknown <TechSupp@EC-CTR-AFS13.itc.mashhad.ir>
2026-06-21 13:04:43 +0330

    modifyied vite.config.json to add support for pdf

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Github-Portfolio/',  // <-- CHANGE THIS
+  base: '/Github-Portfolio/',
+  assetsInclude: ['**/*.pdf'], // <-- Tells Vite to process your research PDFs as static assets
 })
